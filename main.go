8fca5f91c667399eda8e7967c19643eb55d8f7e8
cobyzero <sebastianabalsalazar@gmail.com>
2025-04-16 14:28:43 -0500

    initial golang project

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	var app = fiber.New()
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
+
+}
